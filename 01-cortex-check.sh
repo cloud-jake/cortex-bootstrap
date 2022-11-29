ceb0d24a12188b7e28dc59d6218732cad8f4cac7
cloud-jake <jake.holmquist@kinandcarta.com>
2022-11-28 20:13:27 -0500

    fix variables

--- 01-cortex-check.sh
+++ 01-cortex-check.sh
@@ -2,7 +2,9 @@
 
 #https://github.com/GoogleCloudPlatform/cortex-data-foundation#check-setup
 
-PROJECT_ID=partarch-cortex-poc-6
+#PROJECT_ID=$PJID_SRC
+
+source variables.inc
 
 #Clone checker repo
 git clone  https://github.com/fawix/mando-checker
@@ -13,5 +15,5 @@ cd mando-checker
 #run the checker
 gcloud builds submit \
    --project $PROJECT_ID  \
-   --impersonate-service-account service-account-cortex@${PROJECT_ID}.iam.gserviceaccount.com \
+   --impersonate-service-account ${SERVICE_ACCOUNT}@${PJID_SRC}.iam.gserviceaccount.com \
    --substitutions _DEPLOY_PROJECT_ID=${PROJECT_ID},_DEPLOY_BUCKET_NAME=${PROJECT_ID}-dag,_LOG_BUCKET_NAME=${PROJECT_ID}-log .   
